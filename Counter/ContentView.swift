26737d19ab1df67542205ac4717ea87705cf2c3e
Viraj Chitnis <chitnisviraj@gmail.com>
2020-07-13 13:20:59 +0100

    Initial Commit

---
+++ Counter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Counter
+//
+//  Created by Viraj Chitnis on 7/13/20.
+//  Copyright © 2020 Viraj Chitnis. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
