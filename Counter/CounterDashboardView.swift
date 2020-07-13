835fae776ecd257ffda139599682e7457a28e976
Viraj Chitnis <chitnisviraj@gmail.com>
2020-07-13 15:19:34 +0100

    Add new counter to dashboard

--- Counter/CounterDashboardView.swift
+++ Counter/CounterDashboardView.swift
@@ -10,18 +10,42 @@ import SwiftUI
 
 struct CounterDashboardView: View {
     @State private var counters: [Counter] = []
+    @State private var addCounter: Bool = false
+    @State private var newCounterName: String = ""
+    @State private var newCounterDescription: String = ""
+    @State private var newCounterCount: String = ""
     
     var body: some View {
         NavigationView {
             ScrollView {
                 ForEach(counters, id: \.id) { counter in
                     CounterItemView(counter: counter)
+                        .frame(maxWidth: .infinity)
                 }
                 .padding(.all)
-                Button(action: {}) {
-                    HStack {
-                        Image(systemName: "plus")
-                        Text("Counter")
+                if self.addCounter {
+                    VStack {
+                        TextField("Name", text: $newCounterName)
+                            .multilineTextAlignment(.center)
+                        TextField("Description (optional)", text: $newCounterDescription)
+                            .multilineTextAlignment(.center)
+                        TextField("Starting amount", text: $newCounterCount)
+                            .keyboardType(.numberPad)
+                            .autocapitalization(.none)
+                            .disableAutocorrection(true)
+                            .multilineTextAlignment(.center)
+                        Button(action: addNewCounter) {
+                            Text("Done")
+                        }
+                    }
+                } else {
+                    Button(action: {
+                        self.addCounter = !self.addCounter
+                    }) {
+                        HStack {
+                            Image(systemName: "plus")
+                            Text("Counter")
+                        }
                     }
                 }
             }
@@ -35,6 +59,17 @@ struct CounterDashboardView: View {
             Counter(name: "Blah 2")
         ]
         self.counters = newCounters
+    }
+    
+    func addNewCounter() {
+        if !self.newCounterName.isEmpty && !self.newCounterCount.isEmpty {
+            let newCounter = Counter(name: self.newCounterName, description: self.newCounterDescription, count: Int(self.newCounterCount) ?? 0)
+            self.newCounterName = ""
+            self.newCounterCount = ""
+            self.counters.append(newCounter)
+        }
+        self.newCounterDescription = ""
+        self.addCounter = !self.addCounter
     }
 }
 
